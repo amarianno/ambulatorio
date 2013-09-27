99e3bf56d0885a0fe8bf6aa06a5a7aac7b1b109d
amarianno <amarianno@gmail.com>
2013-09-27 15:48:10 -0300

    novo sh corrigindo a geracao do bkp e commitando o bkp

--- batch/tarefa_bkp_amb.sh
+++ batch/tarefa_bkp_amb.sh
@@ -1,13 +1,19 @@
 #/bin/bash
-U_PASTA="/var/www/ambulatorio/bkp/"
+#U_PASTA="/var/www/ambulatorio/bkp/"
+U_PASTA="/home/10973787724/Desktop/ambulatorio/bkp/"
 U_DATA=$(/bin/date +%Y%m%d%H%M%S)
 U_CAMINHO="backup-$U_DATA.sql"
 U_HOST="localhost"
 U_USER="root"
-U_PASSWORD="Bu$carta5"
 U_DATABASE="ambulatorio"
 #
 cd $U_PASTA
-mysqldump -h $U_HOST -u $U_USER -p$U_PASSWORD $U_DATABASE > $U_CAMINHO
+mysqldump -h $U_HOST -u $U_USER -p'Bu$carta5' --databases $U_DATABASE > $U_CAMINHO
 gzip $U_CAMINHO
 echo "Backup executado"
+
+#---- GIT
+cd /home/10973787724/Desktop/ambulatorio/bkp/
+git add *
+git commit -m "$U_DATA" 
+git push
